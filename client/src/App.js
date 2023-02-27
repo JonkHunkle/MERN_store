56b4d9b0f86ad10fcf87e05945297594e9cb8dee
JonkHunkle <88416486+JonkHunkle@users.noreply.github.com>
2023-02-26 17:25:42 -0800

    implement router

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ProductComponent from './components/ProductComponent';
 // Importing our theme provider which will make our global state available to child components
 import StoreProvider from './utils/StoreContext';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+
 // import { getMainDefinition } from '@apollo/client/utilities';
 // import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 // import { createClient } from 'graphql-ws';
@@ -40,7 +43,12 @@ export default function App() {
   return (
     <ApolloProvider client={client}> 
     <StoreProvider>
-      <ProductComponent/>
+      <Router>
+        <Routes>
+
+        <Route exact path='/' element = {<ProductComponent/>} />
+        </Routes>
+      </Router>
     </StoreProvider>
     </ApolloProvider>
   );
